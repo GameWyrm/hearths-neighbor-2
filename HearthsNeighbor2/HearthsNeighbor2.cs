04c96773bc85df6223f9381635060a5ecb8e1fd2
GameWyrm <daronan97@hotmail.com>
2024-03-13 17:44:56 -0700

    initial commit

---
+++ HearthsNeighbor2/HearthsNeighbor2.cs
@@ -0,0 +1,33 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace HearthsNeighbor2
+{
+    public class HearthsNeighbor2 : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(HearthsNeighbor2)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+
+}
