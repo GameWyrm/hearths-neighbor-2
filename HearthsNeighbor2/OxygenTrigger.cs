c47e5d5d2c01f04730af2528acfb3ff21efb0ed1
GameWyrm <daronan97@hotmail.com>
2024-03-17 23:35:30 -0700

    Light and dialogue

---
+++ HearthsNeighbor2/OxygenTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HearthsNeighbor2
+{
+    public class OxygenTrigger : MonoBehaviour
+    {
+        public bool turnOnOxygen = false;
+        public TimeStateManager timeManager;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                timeManager.SetOxygenState(turnOnOxygen);
+            }
+        }
+    }
+}
---
+++ HearthsNeighbor2/TimeState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthsNeighbor2
+{
+    public enum TimeState
+    {
+        /// <summary>
+        /// First 8 minutes, all systems online
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// 8-14 minutes, gravity weakens, starts at 14 minutes remaining
+        /// </summary>
+        LowGrav,
+        /// <summary>
+        /// 14-19 minutes, gravity off, starts at 8 minutes remaining
+        /// </summary>
+        NoGrav,
+        /// <summary>
+        /// 19-21 minutes, red lights during emergency power, starts at 3 minutes remaining
+        /// </summary>
+        RedLights,
+        /// <summary>
+        /// 20+ minutes, power completely dead, starts at 2 minutes remaining
+        /// </summary>
+        Dead
+    }
+}
