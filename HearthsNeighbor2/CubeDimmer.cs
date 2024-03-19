6a9c58105847c7e68c8c80d7027d70529bbd3a8d
GameWyrm <daronan97@hotmail.com>
2024-03-18 21:46:58 -0700

    Device model

---
+++ HearthsNeighbor2/CubeDimmer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HearthsNeighbor2
+{
+    public class CubeDimmer : MonoBehaviour
+    {
+        public MeshRenderer mesh;
+
+        private SingleInteractionVolume _interactVolume;
+        private bool hasBeenRead;
+
+        private void Awake()
+        {
+            _interactVolume = this.GetRequiredComponent<SingleInteractionVolume>();
+            _interactVolume.OnPressInteract += (() =>
+            {
+                if (!hasBeenRead) GlobalMessenger.AddListener("ExitConversation", () =>
+                {
+                    if (hasBeenRead) return;
+                    mesh.materials[1].color = Color.black;
+                    mesh.transform.GetChild(0).gameObject.SetActive(false);
+
+                    hasBeenRead = true;
+                });
+            });
+        }
+    }
+}
