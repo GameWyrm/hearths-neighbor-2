8d681d8198b8d3c10530ac247e8b20828d1fdf2a
GameWyrm <daronan97@hotmail.com>
2024-03-23 00:51:29 -0700

    Library

---
+++ HearthsNeighbor2/BookItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HearthsNeighbor2
+{
+    public class BookItem : OWItem
+    {
+        public BookReceiver receiver;
+
+        private bool isReturned = false;
+        private INewHorizons nh;
+
+        private void Start()
+        {
+            nh = HearthsNeighbor2.Main.newHorizons;
+            receiver.books.Add(gameObject);
+        }
+
+        public override string GetDisplayName()
+        {
+            return nh.GetTranslationForOtherText("$HN2ItemBook");
+        }
+
+        public override void DropItem(Vector3 position, Vector3 normal, Transform parent, Sector sector, IItemDropTarget customDropTarget)
+        {
+            base.DropItem(position, normal, parent, sector, customDropTarget);
+            receiver.CheckBooks(gameObject, out isReturned);
+            if (isReturned)
+            {
+                _interactable = false;
+            }
+        }
+    }
+}
