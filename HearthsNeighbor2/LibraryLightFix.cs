bf309d5dd86ec4caf969159b2056a313d80d19a2
GameWyrm <daronan97@hotmail.com>
2024-04-16 19:03:57 -0700

    More text and minor edits

---
+++ HearthsNeighbor2/LibraryLightFix.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HearthsNeighbor2
+{
+    /// <summary>
+    /// Fix for the lighting bug
+    /// </summary>
+    public class LibraryLightFix : MonoBehaviour
+    {
+        private Light light;
+
+        private void Start()
+        {
+            light = GetComponent<Light>();
+            light.shadowBias = -1;
+            light.shadowNearPlane = 1;
+            ChangeLightQuality(PlayerData.GetGraphicSettings());
+            GlobalMessenger<GraphicSettings>.AddListener("GraphicSettingsUpdated", ChangeLightQuality);
+        }
+
+        private void ChangeLightQuality(GraphicSettings settings)
+        {
+            if (light == null) light = GetComponent<Light>();
+            switch (settings.shadowQuality)
+            {
+                case ShadowQuality.LOW:
+                    light.shadowCustomResolution = 1000;
+                    break;
+                case ShadowQuality.MEDIUM:
+                    light.shadowCustomResolution = 2000;
+                    break;
+                case ShadowQuality.HIGH:
+                    light.shadowCustomResolution = 5000;
+                    break;
+                case ShadowQuality.VERY_HIGH:
+                    light.shadowCustomResolution = 10000;
+                    break;
+                default:
+                    light.shadowCustomResolution = 1000;
+                    break;
+            }
+        }
+    }
+}
